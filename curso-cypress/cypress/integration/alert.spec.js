7bff4e03fa6971889af08362691f7412d5c8bad2
phf_dev <pedrohfilho.dev@gmail.com>
2022-11-28 21:13:55 -0400

    mocks, alerts, iframes e popups

---
+++ curso-cypress/cypress/integration/alert.spec.js
@@ -0,0 +1,87 @@
+///<reference types="cypress" />
+
+describe('Work with Alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+
+    it('Alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+
+    it.only('Alert  com mock', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+        })
+    })
+
+    it.only('Confirm', () => {
+        cy.get('#confirm').click()
+        cy.on('window:confirm', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirmado')
+        })
+    })
+
+    it.only('Deny', () => {
+        cy.get('#confirm').click()
+        cy.on('window:confirm', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Negado')
+        })
+    })
+
+
+    it.only('Prompt', () => {
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('42')
+        })
+        cy.on('window:confirm', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Era 42?')
+        })
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+    })
+
+    it.only('Validando mensagens', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+        cy.get('#formNome').type('João', { delay: 150 })
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+        cy.get('[data-cy=dataSobrenome]').type('Pedro', { delay: 150 })
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
+
+})
