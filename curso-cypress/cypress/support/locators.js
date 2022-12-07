d1727423ab09a5cee959b476c52242e716f641ed
phf_dev <pedrohfilho.dev@gmail.com>
2022-12-06 20:33:23 -0400

    feat real aplication tests

--- curso-cypress/cypress/support/locators.js
+++ curso-cypress/cypress/support/locators.js
@@ -5,14 +5,34 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        HOME: '[data-test=menu-home]',
         SETTINGS: '[data-test=menu-settings]',
         CONTAS: '[href="/contas"]',
-        RESET: '[href="/reset"]'
+        RESET: '[href="/reset"]',
+        MOVIMENTACAO: '[data-test=menu-movimentacao]',
+        EXTRATO: '[data-test=menu-extrato]'
     },
     CONTAS: {
         NOME: '[data-test=nome]',
         BTN_SALVAR: '.btn',
-        XP_BTN_ALTERAR: "//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']"
+        FN_XP_BTN_ALTERAR: (nome) => `//table//td[contains(., '${nome}')]/..//i[@class='far fa-edit']`
+    },
+    MOVIMENTACAO: {
+        DESCRICAO: '[data-test=descricao]',
+        VALOR: '[data-test=valor]',
+        INTERESSADO: '[data-test=envolvido]',
+        STATUS: '[data-test=status]',
+        CONTA: '[data-test=conta]',
+        BTN_SALVAR: '.btn-primary'
+    },
+    EXTRATO: {
+        LINHAS: '.list-group > li',
+        FN_XP_BUSCA_ELEMENTO: (desc, value) =>  `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
+        FN_XP_REMOVER_ELEMENTO: (conta) => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-trash-alt']`,
+        FN_XP_ALTERAR_ELEMENTO: (conta) => `//span[contains(., '${conta}')]/../../..//i[@class='fas fa-edit']`
+    },
+    SALDO: {
+        FN_XP_SALDO_CONTA: (nome) => `//td[contains(., '${nome}')]/../td[2]`,
     },
     MESSAGE: '.toast-message'
 }
